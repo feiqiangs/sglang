e762dc0d0e9823ff56aba1e6d0709bab5a7866ea
zhuofan1123 <zhuofanl@nvidia.com>
2026-04-10 07:21:56 +0000

    support prefetch in flexkv-connector

--- python/sglang/srt/managers/scheduler.py
+++ python/sglang/srt/managers/scheduler.py
@@ -2423,7 +2423,7 @@ class Scheduler(
                 ):
                     break
 
-            if self.enable_hicache_storage:
+            if self.enable_hicache_storage or self.enable_kv_connector:
                 prefetch_done = self.tree_cache.check_prefetch_progress(req.rid)
                 if not prefetch_done:
                     # skip staging requests that are ongoing prefetch
